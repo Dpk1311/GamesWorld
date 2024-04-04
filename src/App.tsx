c7ef9899835fd8b1eb1fd4f6c10f3edc4fe47033
Deepak S <s.deepak73333@gmail.com>
2024-04-04 20:11:13 +0530

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { Button, ButtonGroup } from '@chakra-ui/react'
+
+function App() {
+
+  return (
+    <>
+      <Button colorScheme='green'>Button</Button>
+    </>
+  )
+}
+
+export default App

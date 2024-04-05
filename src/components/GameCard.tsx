15613ccec58d6d92592c3d701a454a0a87014a40
Deepak S <s.deepak73333@gmail.com>
2024-04-06 01:02:04 +0530

    Code Refactored

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -1,5 +1,5 @@
 import { Game } from "../hooks/useGameFetch";
-import { Card, CardBody, HStack, Heading, Image,Text } from "@chakra-ui/react";
+import { Card, CardBody, HStack, Heading, Image} from "@chakra-ui/react";
 import PlatformItemList from "./PlatformItemList";
 import GameCriticScore from "./GameCriticScore";
 import getImageCroppedUrl from "../services/image-url";
@@ -10,7 +10,7 @@ interface Props {
 
 const GameCard = ({ game }: Props) => {
   return (
-    <Card borderRadius={10} overflow='hidden'>
+    <Card>
         <Image src={getImageCroppedUrl(game.background_image)} />
         <CardBody>
             <Heading fontSize='2xl'>{game.name}</Heading>
--- src/components/GameFetch.tsx
+++ src/components/GameFetch.tsx
@@ -2,6 +2,7 @@ import { SimpleGrid, Text } from "@chakra-ui/react";
 import useGameFetch from "../hooks/useGameFetch";
 import GameCard from "./GameCard";
 import GameCardSkeleton from "./GameCardSkeleton";
+import GameCardContainer from "./GameCardContainer";
 
 const GameFetch = () => {
   const { err, games, isLoading } = useGameFetch();
@@ -10,16 +11,17 @@ const GameFetch = () => {
   return (
     <>
       {err && <Text>{err}</Text>}
-      <SimpleGrid
-        columns={{ sm: 1, md: 2, lg: 3, xl: 5 }}
-        padding="10px"
-        spacing={10}
-      >
+      <SimpleGrid columns={{ sm: 1, md: 2, lg: 3 }} padding="10px" spacing={10}>
         {isLoading &&
-          Skeletons.map((Skeleton) => <GameCardSkeleton key={Skeleton} />
-          )}
+          Skeletons.map((Skeleton) => (
+            <GameCardContainer>
+              <GameCardSkeleton key={Skeleton} />
+            </GameCardContainer>
+          ))}
         {games.map((game) => (
-          <GameCard key={game.id} game={game} />
+          <GameCardContainer>
+            <GameCard key={game.id} game={game} />
+          </GameCardContainer>
         ))}
       </SimpleGrid>
     </>
